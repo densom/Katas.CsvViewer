8fbb5a586a7c8aff74ee9ca169f92a3d4301f5c5
Dennis Somerville <dennis.somerville@gmail.com>
2013-10-21 19:59:13 -0400

    Can add headers and one row.

---
+++ CsvViewer.Tests/DataTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace CsvViewer.Tests
+{
+    [TestFixture]
+    public class DataTests
+    {
+        [Test]
+        public void AddHeader_SetsHeaderProperty_ToArrayOfMatchingStrings()
+        {
+            var data = new Data();
+            data.SetHeader("h1", "h2");
+
+            Assert.That(data.Header, Is.EqualTo(new[] {"h1", "h2"}));
+        }
+
+        [Test]
+        public void AddRow_SetsFirstRow_EqualToValuesPassedIn()
+        {
+            var data = new Data();
+            data.AddRow("r1-c1", "r1-c2");
+            Assert.That(data.Rows.First(), Is.EqualTo(new [] {"r1-c1", "r1-c2"}));
+        }
+    }
+}
---
+++ CsvViewer/Data.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace CsvViewer
+{
+    public class Data
+    {
+        private string[] _rows;
+
+        public void SetHeader(params string[] headerValues)
+        {
+            Header = headerValues;
+        }
+
+        public string[] Header { get; private set; }
+
+        public IEnumerable<string[]> Rows
+        {
+            get
+            {
+                return new List<string[]> {_rows};
+            }
+        }
+
+        public void AddRow(params string[] rowValues)
+        {
+            _rows = rowValues;
+        }
+    }
+}
