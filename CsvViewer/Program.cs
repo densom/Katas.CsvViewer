e4228b7d3ef5def81c1cbb36032f8c911cd6b8da
Dennis Somerville <dennis.somerville@gmail.com>
2013-10-25 23:36:26 -0400

    Refactored.  Implemented first command.

--- CsvViewer/Program.cs
+++ CsvViewer/Program.cs
@@ -9,20 +9,18 @@ namespace CsvViewer
     class Program
     {
         static IDataSource dataSource = new CsvFileDataSource("persons.csv");
-
         static DataFormatter formatter = new DataFormatter(new Data(dataSource));
 
         static void Main(string[] args)
         {
+            WriteTable(1);
+            KeypressLoop();
+        }
 
-
-
-
+        private static void KeypressLoop()
+        {
             ConsoleKey keypress;
-
             var currentPage = 1;
-
-            WriteTable(currentPage);
 
             do
             {
@@ -36,20 +34,20 @@ namespace CsvViewer
                     case ConsoleKey.P:
                         WriteTable(--currentPage);
                         break;
+                    case ConsoleKey.F:
+                        currentPage = 1;
+                        WriteTable(currentPage);
+                        break;
+                    case ConsoleKey.L:
+                        throw new NotImplementedException("need to implement page count");
                 }
-
-
-
             } while (keypress != ConsoleKey.X);
-
         }
 
         private static void WriteTable(int page)
         {
             Console.Clear();
-            Console.WriteLine(formatter.GetHeaderString(page));
-            Console.WriteLine(formatter.GetSeparatorString(page));
-            formatter.GetRowStrings(page).ToList().ForEach(Console.WriteLine);
+            Console.WriteLine(formatter.GetTable(page));
             Console.WriteLine();
             Console.WriteLine("N(ext page, P(revious page, F(irst page, L(ast page, eX(it");
         }
